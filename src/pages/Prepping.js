3fd0ab26ad6bd8d448522ea323ed5de05234a86b
palhamel <pal.hamel@gmail.com>
2020-06-06 12:44:00 +0200

    Add text about prepping

--- src/pages/Prepping.js
+++ src/pages/Prepping.js
@@ -1,11 +1,150 @@
-import React from 'react'
+import React from "react";
 
 export const Prepping = () => {
   return (
     <div>
       <h2>Prepping info page</h2>
-      <p>..............</p>
-
+      <p>
+        Att vara förberedd Ordet ”prepping” betyder ”förberedelse” och är inte
+        något nytt fenomen, det handlar helt enkelt om att förbereda sig på en
+        situation där viktiga samhällsfunktioner slås ut. Om det är strömavbrott
+        en längre tid kan det vara bra att kunna laga mat eller lyssna på
+        vevradio i väntan på att elen kommer tillbaka.
+      </p>
+      <p>
+        Friluftsmänniskor har ofta en stor del av den utrustning som krävs för
+        att hantera en sån situation. Fundera över var du förvarar din
+        utrustning, den bör vara lättåtkomlig.
+      </p>
+      <p>
+        Det är fem element som behövs för att hantera en nödsituation i hemmet:
+      </p>
+      <h3>Information, belysning, mat, vatten och värme</h3>
+      <h4>Information</h4>
+      <p>
+        Ha en radio som drivs av batterier lättillgängligt, att få information
+        om vad som hänt är viktigt, spekulationer kan leda till stark oro.
+      </p>
+      <h4>Belysning</h4>
+      <p>
+        Reservbelysning kan användas i form av pannlampa, lykta och värmeljus.
+        En pannlampa är alltid bra för då kan du använda båda händerna och
+        samtidigt enkelt rikta ljuset. Pannlampor med LED teknik har en lång
+        brinntid och olika ljusstyrkor som förbrukar olika mycket effekt.
+      </p>
+      <h4>Mat</h4>
+      <p>
+        Vid ett längre elavbrott kan man laga mat på camping- och friluftskök.
+        Glöm inte att alltid ha bränsle för ditt kök hemma. Placera inte köket
+        på spisen under köksfläkten – fettet i fläkten kan fatta eld, se upp för
+        bränslespill och ha alltid en hink med vatten i närheten. Engångsgrillar
+        och kolgrillar får inte användas inomhus på grund av fara för koldioxid
+        och brand.
+      </p>
+      <h4>Matförråd</h4>
+      <p>
+        Ett oväder eller elavbrott kommer plötsligt och då är det bra att ha
+        matvaror som kräver enkel och energisnål tillagning hemma. Man behöver
+        också kunna ta hand om den mat som finns i kyl och frys. Ha alltid ett
+        reservförråd av mat hemma. Men använd det hela tiden så att varorna
+        omsätts och inte är äldre än ett halvt år. Frystorkad mat har lång
+        hållbarhet och går snabbt att tillaga, allt som behövs är lite kokande
+        vatten. Kom ihåg att spädbarn och personer med särskild diet behöver
+        lämplig kost. Förslag på livsmedel i reservförrådet, räcker för en
+        person i en vecka: – Kött- och fiskkonserver, 1000 g –
+        Grönsakskonserver, 2000 g – Ris eller pasta, 500 g – Potatismospulver,
+        500 g – Torkade bönor och linser (kan även groddas), 500 g – Gryn,
+        flingor, (vetemjöl), 500 g – Hårt bröd, skorpor och kex, 1000 g – Smör,
+        margarin, 500 g – Soppor (pulver / burk), 500 g – Torrmjölk, 500 g –
+        Saft eller juice, ca 3 liter färdig
+      </p>
+      <h4>Vatten</h4>
+      <p>
+        Ett elavbrott påverkar även vattenförsörjningen. Vattenverket och pumpar
+        längs vattennätet behöver el för att fungerar. Högt belägna områden kan
+        bli utan vatten redan efter några timmar. Det är då kommunens uppgift
+        att ordna med dricksvatten vid olika tappställen. Till ett bostadsområde
+        utan vatten kan vatten köras i tankbil och speciella hämtställen ordnas,
+        vanligen med hjälp av brandförsvar eller gatukontor. Vattnet hämtas
+        lättast i plastdunkar eller hinkar. Ha helst en dunk med skruvlock och
+        kran. Du kan själv ordna dricksvatten om du inte får det utkört! Använd
+        sjövatten och vatten från friska brunnar, vattendrag och bäckar som du
+        säkert vet att är fria från giftigt utsläpp. Vattnet renar du så här: –
+        vattenreningstabletter – sila bort småpartiklar – koka vattnet utan lock
+        i minst fem minuter Även i tanken på toaletten finns reservvatten – men
+        glöm inte att rena det!
+      </p>
+      <h4>Värme</h4>
+      <p>
+        En bostad kyls snabbt ut och blir snart obehaglig att vistas i. Redan en
+        temperatur på 15 grader kan kännas ruggig.
+      </p>
+      <h4>Nödrum</h4>
+      <p>
+        Förbered något rum i bostaden som ett nödrum. Ett maximalt bra nödrum
+        ligger: – mellan andra rum – mot söder – i övervåningen I flerfamiljshus
+        väljer du rummet intill eller ovanpå/under det rum din granne håller
+        varmt. Stäng till mot övriga kalla rum.
+      </p>
+      <h4>Tälta inomhus</h4>
+      <p>
+        Ta fram tältet, spänn upp det och fäst i stadiga föremål i rummet. Eller
+        bygg en koja med hjälp av stolar, soffa och bord. Inuti tältet eller
+        kojan hålls värmen kvar. Lägg mattor på golven i kalla rum. Ha
+        ordentligt på fötterna. Isolera fönstren – särskilt på natten – med
+        gardiner, filtar mm.
+      </p>
+      <h4>Kläder</h4>
+      <p>
+        Klä dig med flera olika lager kläder. Lagret längst in mot kroppen och
+        mellanlagret ska hålla kroppen varm. Det yttersta lagret kläder ska
+        skydda mot blåst, regn och snö när du går utomhus. Natten klarar man bra
+        om man sover i sovsäck.
+      </p>
+      <h4>Extra värme</h4>
+      <p>
+        Det är bra att kunna värma upp bostaden eller något rum i den på något
+        annat sätt än med el när det blir strömavbrott. För att få extra värme
+        kan du: – bränna stearinljus eller fotogenlampa med veke – använda
+        fotogenkamin eller gasolvärmeapparat – varje människa avger 100W
+      </p>
+      <h4>Koloxidfara</h4>
+      <p>
+        Men tänk på koloxidfaran. Vädra kort men rejält och ofta om du använder
+        kok- eller värmeapparater, som förbrukar mycket syre. En stearinljuslåga
+        är en bra mätare på om det finns syre. Minskar lågan och blir den
+        småningom flämtande måste du genast vädra in frisk luft.
+      </p>
+      <h4>Brandrisk</h4>
+      <p>
+        Se upp med ljus i fönster! – Håll bränslen och öppna lågor åtskilda! –
+        Förvara inte mer än tillåten mängd bränslen i bostaden. – Förvara
+        bränsle i godkända kärl. – Ha alltid brandsläckningsutrustning
+        tillgänglig. Innan strömmen kommer tillbaka ska du stänga av alla
+        elelement och andra större strömförbrukare. När strömmen återkommer slår
+        du i mycket långsam takt på en apparat i taget.
+      </p>
+      <h4>Överlevnadsburken</h4>
+      <p>
+        Räddaren i nöden vid långa elavbrott! Det kan vara svårt att i mörkret
+        hitta eller komma ihåg var tändstickor, ljus och ficklampan finns. Ett
+        bra råd är att samla alla ”överlevnadssaker” på en plats som hela
+        familjen känner till. Man kan till exempel ha en ”överlevnadsburk” som
+        innehåller det viktigaste för att klara ett elavbrott. Välj de saker ur
+        listan nedan du tror din familj behöver vid ett elavbrott. Lägg dem i
+        burken och ställ den på en plats som alla i familjen känner till.
+        Överlevnadsburken kan innehålla: – Batterier i reserv – Choklad –
+        Multiverktyg, mejsel och tång – Ficklampa – Kaffepulver och tepåsar –
+        Kontanter (sedlar) – Papper och penna – Plast- och fryspåsar – Proppar –
+        Radio, batteridriven – Reservmedicin – Reservnycklar – Socker och
+        druvsocker – Spel: kortlek, litet spel, ex. Yatzy – Tändstickor –
+        Vattenfilter – Värmeljus I riktigt extrema lägen kan burken även
+        användas att koka på! Använd då en plåtburk, ca 15 cm hög och ca 30 cm
+        bred. Borra 20-30 hål med diametern 10 mm i locket samt även några i
+        sidan. Ställ tre värmeljus i burken och lägg på locket. Hålen ger syre
+        till ljusen. Sen tar det en och halv timme och du har varmt vatten.
+      </p>
+      Källa: Civilförsvarsförbundet, www.civil.se, Lars Fält och Naturkompaniet
     </div>
-  )
-}
+  );
+};
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const Start = () => {
   return (
     <div>
       <h2>Start page</h2>
-      <p>..............</p>
-      
+      <p>
+        Förberedd för oväder och strömavbrott! Människan blir mer och mer utsatt
+        och sårbar i samhället, den moderna infrastrukturen och teknikens
+        framfart gör oss mer och mer beroende. Vad händer när vi blir strömlösa,
+        insnöade eller översvämmade?.
+      </p>
+      <p>
+        En bra början kan vara att se till att det finns ett alternativt sätt
+        att laga mat hemma.
+      </p>
+      <p>
+        Oavsett om du är en rutinerad prepper, en nyfiken nybörjare eller bara
+        vill vara lite förberedd så har vi ett brett sortiment av smarta
+        produkter som gör livet lite lättare om något oväntat händer.
+      </p>
     </div>
-  )
-}
+  );
+};
