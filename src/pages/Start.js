76a74732f0c9e1bf3484bb7a9811c33249fda268
Pål Hamel <pal.hamel@gmail.com>
2020-06-18 13:34:42 +0200

    work on page typography

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,24 +1,37 @@
 import React from "react";
+// import dayjs from 'dayjs'
+
+import "../pages/start_style.css";
 
 export const Start = () => {
   return (
     <div>
-      <h2>Varför prepping? (Start page)</h2>
-      <p>
-        Förberedd för oväder och strömavbrott! Människan blir mer och mer utsatt
-        och sårbar i samhället, den moderna infrastrukturen och teknikens
-        framfart gör oss mer och mer beroende. Vad händer när vi blir strömlösa,
-        insnöade eller översvämmade?.
-      </p>
-      <p>
-        En bra början kan vara att se till att det finns ett alternativt sätt
-        att laga mat hemma.
-      </p>
-      <p>
-        Oavsett om du är en rutinerad prepper, en nyfiken nybörjare eller bara
-        vill vara lite förberedd så har vi ett brett sortiment av smarta
-        produkter som gör livet lite lättare om något oväntat händer.
-      </p>
+      <section className="container-page">
+        {/* <time dateTime="" className="date">
+          Local Date Here
+        </time> */}
+
+        <h2>Varför prepping? (Start page)</h2>
+        <p>Förberedd för oväder och strömavbrott! </p>
+        <p>
+          Människan blir mer och mer utsatt och sårbar i samhället, den moderna
+          infrastrukturen och teknikens framfart gör oss mer och mer beroende.
+          <span className="text-highlight">Vad händer när vi blir strömlösa, insnöade eller översvämmade?</span>
+        </p>
+
+        <section className="text-box-focus">
+          <h1>
+            En bra början kan vara att se till att det finns ett alternativt
+            sätt att laga mat hemma.
+          </h1>
+        </section>
+
+        <p>
+          Oavsett om du är en rutinerad prepper, en nyfiken nybörjare eller bara
+          vill vara lite förberedd så har vi ett brett sortiment av smarta
+          produkter som gör livet lite lättare om något oväntat händer.
+        </p>
+      </section>
     </div>
   );
 };
