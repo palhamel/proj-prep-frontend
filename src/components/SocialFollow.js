59d9780635c20c54b830d9e013fade40996d21d4
Pål Hamel <pal.hamel@gmail.com>
2020-06-23 16:10:22 +0200

    Update social links with target and rel referrer

--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -5,25 +5,35 @@ import './socialfollow.css'
 
 export const SocialFollow = () => {
   return (
-    <div className="social-container">
+    <section className="social-container">
       <a
         href="https://www.youtube.com/"
-        className="youtube social">
+        className="youtube social"
+        target="_blank"
+        rel="noopener noreferrer">
         <FontAwesomeIcon icon={faYoutube} size="lg" />
       </a>
       <a
         href="https://www.facebook.com/"
-        className="facebook social">
+        className="facebook social"
+        target="_blank"
+        rel="noopener noreferrer">
         <FontAwesomeIcon icon={faFacebook} size="lg" />
       </a>
-      <a href="https://www.twitter.com/" className="twitter social">
+      <a
+        href="https://www.twitter.com/"
+        className="twitter social"
+        target="_blank"
+        rel="noopener noreferrer">
         <FontAwesomeIcon icon={faTwitter} size="lg" />
       </a>
       <a
         href="https://www.instagram.com/"
+        target="_blank"
+        rel="noopener noreferrer"
         className="instagram social">
         <FontAwesomeIcon icon={faInstagram} size="lg" />
       </a>
-    </div>
+    </section>
   )
 }
