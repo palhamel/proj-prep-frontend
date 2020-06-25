d68885b6ae7e212cc34b7877dd11064539a1ddba
Pål Hamel <pal.hamel@gmail.com>
2020-06-25 19:49:27 +0200

    Update SVT news fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { Home } from './pages/Home'
 // Keeping it clean here
-// const dotenv = require('dotenv').config()
-require('dotenv').config()
-
+// require('dotenv').config()
 export const App = () => {
   return (
     <div>
