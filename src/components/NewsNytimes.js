a65e56b182dacafb8d73dce5a804defb9966ed30
Pål Hamel <pal.hamel@gmail.com>
2020-06-26 11:48:36 +0200

    Update NY API

--- src/components/NewsNytimes.js
+++ src/components/NewsNytimes.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import dayjs from 'dayjs'
 import '../pages/kitlist_style.css'
-// import { newsApiKeyNyTimes } from '../apiKeys'
 
 const apiKey = process.env.REACT_APP_API_KEY_NYT
 
 export const NewsNytimes = () => {
-  // const apiURL = `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${newsApiKeyNyTimes}`
   const apiURL = `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${apiKey}`
   const [newsNy, setNews] = useState([])
 
@@ -15,12 +13,11 @@ export const NewsNytimes = () => {
       .then((res) => res.json())
       .then((json) => setNews(json.results))
   }, [apiURL])
-  console.log(newsNy)
 
   return (
     <main className="kits-container">
       <h2>NY Times, world news:</h2>
-      {newsNy.slice(0, 5).map((news) => (
+      {newsNy.slice(0, 4).map((news) => (
         <article className="kit-card effect2" key={news.published_date}>
           <p className="kit-details">
             {dayjs(news.published_date).format('YYYY-MM-DD')}
