2d585c4b53cfeae329bbf3f8fa0dec74aea1c0d5
Pål Hamel <pal.hamel@gmail.com>
2020-06-27 17:08:35 +0200

    Update page NotFound with gif

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -6,9 +6,10 @@ export const NotFound = () => {
   return (
     <section className="kits-container">
       <h1>404 - Något är knas!</h1>
-      <Link to="/" className="link-text-inline">
+      <Link to="/" className="link-text-inline" style={{marginTop: "25px"}}>
         Klicka här och börja om från startsidan
       </Link>
+      <img src="https://media.giphy.com/media/VwoJkTfZAUBSU/giphy.gif" alt="upps" style={{marginTop: "20px"}}/>
     </section>
   )
 }
